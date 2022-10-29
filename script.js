40f7eae8e559ed56115ca591fe003e32031cea70
Bob Klasen <robfklasen@gmail.com>
2022-10-29 11:12:27 -0500

    First Major Commit: Main architecture of classes added with JS randomizer in place to add cards dynamicly.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 'use strict';
+/* --------------------------- MAIN --------------------------- */
 
-// JavaScript Selectors
+// Main Selectors
 
 const grid = document.querySelector('.grid');
 const cardHeader = document.querySelector('.card_header');
@@ -13,19 +14,16 @@ const cardsArrayOfObjects = [
   { header: 'header-2', text: 'string of text 2', btn: 'Check' },
   { header: 'header-3', text: 'string of text 3', btn: 'Send' },
   { header: 'header-4', text: 'string of text 4', btn: 'Call' },
-  { header: 'header-5', text: 'string of text 5' },
-  { header: 'header-6', text: 'string of text 6' },
-  { header: 'header-7', text: 'string of text 7' },
-  { header: 'header-8', text: 'string of text 8' },
+  { h2: ':) Click and Smile', cardID: 'card-smile' },
 ];
 
 const testBtn = headerBtn.addEventListener('click', function () {
   console.log('teset');
   cardStyle2.innerHTML = 'test';
 });
-
 //Assigns each card an ID and displays it in random order
 const loadAllCards = function (array) {
+  console.log(array);
   const arrayCopy = array.slice();
   for (let i = 0; i < arrayCopy.length; i++) {
     arrayCopy[i].id = i + 1;
@@ -34,37 +32,47 @@ const loadAllCards = function (array) {
     const random = Math.floor(Math.random() * arrayCopy.length);
     const el = arrayCopy.splice(random, 1)[0];
     let html;
-    if (el.hasOwnProperty('btn')) {
+    console.log(Object.keys(el));
+    if (el.hasOwnProperty('cardID')) {
+      console.log('test');
       html = `
-      <div class="card_style3 card-universal">
-        <div class="card_style3-contentContainer"> 
-          <div class="card-h2">${el.header}</div>
-          <div class="card-text">${el.text}</div>
-          <div class="card-number">${el.id}</div>
-          <div class="card-btn">${el.btn}</div>
+        <div class="card_style2 gridCard-universal" id="cardHeart">
+          <div class="card_style2-contentContainer contentCard-universal">
+            <h2 class="card-heart-h2">
+              :) Click and Smile
+              <i class="card-heart fas fa-heart"></i>
+              <span class="card-heartLikeCount" id="heartCount">0</span>
+            </h2>
+          </div>
         </div>
-      </div>
-  `;
-      cardStyle1.insertAdjacentHTML('afterend', html);
-    } else {
-      html = `
-      <div class="card_style2 card-universal">
-        <div class="card_style2-contentContainer"> 
-          <div class="card-h2">${el.header}</div>
-          <div class="card-text">${el.text}</div>
-          <div class="card-number">${el.id}</div>
-        </div>
-      </div>
   `;
       cardStyle1.insertAdjacentHTML('afterend', html);
     }
   }
 };
 
-loadAllCards(cardsArrayOfObjects);
+/* --------------------------- CARDS --------------------------- */
+
+/* ------------------- Smile ------------------ */
+
+// Selectors
+const heartCard = document.querySelector('#cardHeart');
+const heartCount = document.getElementById('heartCount');
+
+// Variables
+let i = 0;
+
+// Event Listeners
+heartCard.addEventListener('click', function () {
+  i++;
+  heartCount.innerText = i;
+});
+
 //Randomizers
 const randomInt = (min, max) =>
   Math.floor(Math.random() * (max - min + 1) + min);
 
 const randomColor = () =>
   `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+
+loadAllCards(cardsArrayOfObjects);
